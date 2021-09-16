263c0c36cda049df855354c1ead92f83abbbcfa8
Nicholas Luciw <nicholas.luciw@mail.utoronto.ca>
2021-09-16 18:15:09 -0400

    Multi-line refs working (bug: they are duplicated)

--- code/scripts/utils/parse_html.py
+++ code/scripts/utils/parse_html.py
@@ -6,6 +6,15 @@ def get_title(soup):
 
     return title
 
+def get_link(ref):
+    # Sometimes there's no link
+    try:
+        link = ref.find(class_='external text').get('href')
+    except:
+        link = 'NA'
+    
+    return link
+
 def get_references(soup):
     # Get last ordered list from the HTML file
     references = soup.findAll('ol')[-1]
@@ -15,23 +24,20 @@ def get_references(soup):
 
     # Go through all list items in the ordered list
     for ref in references.findAll('li'):
-        ref_text = ref.find(class_='reference-text')
+        ref_texts = ref.findAll('cite')
 
-        # Sometimes they don't use the reference-text class
-        if ref_text is not None:
-            citations.append(ref_text.text)
-        else:
-            # Sometimes they use a cite class
+        # Sometimes the cite tag is not used
+        if len(ref_texts)!=0:
             for subref in ref.findAll('cite'):
                 citations.append(subref.text)
-
-        # Sometimes there's no link
-        try:
-            link = ref.find(class_='external text').get('href')
-        except:
-            link = 'NA'
-        
-        links.append(link)
+                links.append(get_link(subref))
+        # Sometimes they use this reference-text class
+        elif ref.find(class_='reference-text') is not None:
+            citations.append(ref.find(class_='reference-text').text)
+            links.append(get_link(ref))
+        else:
+            citations.append('Could not parse citation.')
+            links.append('Could not parse citation.')
 
     # Put into DataFrame for easier handling
     df = pd.DataFrame({'Ref #':range(1,1+len(citations)), 
